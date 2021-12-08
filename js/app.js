a0898c854b667897a7bb170dfc70c86cfddece86
Courtney McDonald <courtney.mcdonald2@mavs.uta.edu>
2021-12-07 23:01:31 -0600

    fixed faq questions

--- js/app.js
+++ js/app.js
@@ -14,9 +14,10 @@ toggler.onclick = function(){
     navigation.classList.toggle("open");
 }
 
-/* Dropdown Toggler
-1. Listen for a click on .dropdown > a
-2. Toggle the ".open" class for the navigation
+/*
+    Dropdown Toggler
+    1. Listen for a click on .dropdown > a
+    2. Toggle the ".open" class for the navigation
 */
 
 let dropdownToggler = document.querySelectorAll(".dropdown > a");
@@ -27,3 +28,25 @@ dropdownToggler.forEach(function(dropdown){
         this.closest(".dropdown").classList.toggle("open");
     }
 });
+
+/*
+    FAQ Toggler
+    1. Listen for a click on .question-container a
+    2. Toggle the open class for .answer-container
+*/
+
+let faqToggler = document.querySelectorAll(".question-container");
+
+faqToggler.forEach(function(faq, index){
+    faq.onclick = function(e){
+        e.preventDefault();
+        let answer = document.querySelectorAll(".answer-container");
+        answer.forEach(function(faqAnswer, answerIndex){
+            if (!faqAnswer.classList.contains('open') && index === answerIndex) {
+                faqAnswer.classList.add('open');
+            } else {
+                faqAnswer.classList.remove('open');
+            }
+        })
+    }
+})
